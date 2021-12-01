aef3fc0dda24008c55c0cba24d7856ac54536009
uzairafridii <uzairaziz253@gmail.com>
2021-12-01 13:10:51 +0500

    take full size image

--- app/src/main/java/com/uzair/picturecaptureapp/MainActivity.java
+++ app/src/main/java/com/uzair/picturecaptureapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.uzair.picturecaptureapp;
 
+import static android.os.Environment.getExternalStoragePublicDirectory;
+import static java.util.UUID.randomUUID;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.ActivityResultRegistry;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -7,14 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -28,6 +38,11 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     StorageReference dbStorage;
     Uri imageUri;
     ProgressDialog progressDialog;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
 
         dbStorage = FirebaseStorage.getInstance().getReference().child("TestImages");
-
 
 
         /// click on button to take image using camera
@@ -65,32 +80,46 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.uploadImage)
                 .setOnClickListener(v -> {
                     progressDialog.show();
-                    // Create a reference to "mountains.jpg"
-                    StorageReference mountainsRef = dbStorage.child(imageUri.getLastPathSegment());
-                    mountainsRef.putFile(imageUri)
-                            .addOnSuccessListener(taskSnapshot -> {
-                                progressDialog.dismiss();
-                                Toast.makeText(MainActivity.this, "Successfully uploaded", Toast.LENGTH_SHORT).show();
-                            }).addOnFailureListener(new OnFailureListener() {
+                    String uniqueID = randomUUID().toString();
+                    StorageReference imagePath = dbStorage.child(uniqueID);
+                    imagePath.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Successfully uploaded", Toast.LENGTH_SHORT).show();
+                    }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             progressDialog.dismiss();
-                            Toast.makeText(MainActivity.this, ""+e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
                         }
                     });
 
 
                 });
-        
-
 
 
     }
 
     /// open camera intent
     private void cameraIntent() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.launch(intent);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            BuildConfig.APPLICATION_ID + ".fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    cameraIntent.launch(takePictureIntent);
+                }
+            } catch (IOException ex) {
+                Log.e("DEBUG_TAG", "createFile", ex);
+            }
+        }
 
     }
 
@@ -98,44 +127,93 @@ public class MainActivity extends AppCompatActivity {
     private ActivityResultLauncher<Intent> cameraIntent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == RESULT_OK) {
+                    //get file from file path
+                    File imgFile = new File(mCurrentPhotoPath);
+                    if (imgFile.exists()) {
+                        imageUri = Uri.fromFile(imgFile);
+                        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        try {
+                            Bitmap img = rotateImageIfRequired(myBitmap, imageUri);
+                            imageView.setImageBitmap(img);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
-                    Intent resultIntent = result.getData();
-                    Bitmap imageBitmap = (Bitmap) resultIntent.getExtras().get("data");
-                    Log.d("imageUriCamera", "imageUri: " + imageBitmap);
-                    imageView.setImageBitmap(imageBitmap);
-                    setImageUri(imageBitmap);
-
+                    }
                 }
             });
 
-    private void setImageUri(Bitmap imageBitmap) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(getContentResolver(), imageBitmap, "Image", null);
-        imageUri = Uri.parse(path);
 
+    /// create temp file
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.d("mylog", "Path: " + mCurrentPhotoPath);
+        return image;
+    }
+
+    /// rotate image
+    private Bitmap rotateImageIfRequired(Bitmap img, Uri selectedImage) throws IOException {
+
+        InputStream input = getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
     }
 
     /// request for permission
     private void requestPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAMERA_REQUEST_CODE);
     }
 
     /// check camera permission
     private boolean isPermissionEnable() {
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED) {
+                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
         return false;
     }
 
     /// convert image to byte array
-    private byte[] convertImageToByteArray(Bitmap bitmap) {
+    private void convertImageToByteArray(Bitmap bitmap) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos); // bm is the bitmap object
-        byte[] byteArray = baos.toByteArray();
-        return byteArray;
+        byte[] imageByteArray = baos.toByteArray();
+
     }
 
 
